0570d13794254c67c9c7a7f1ec8dffec64d37af8
kuldeep sharma <kuldeepsharma7768@gmail.com>
2025-04-18 00:52:08 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { HeroSection } from "../components/Layout/UI/HeroSection"
+import { About } from "./About"
+
+export const Home = () => {
+    return (
+        <>
+        <HeroSection />
+        <About />
+
+        
+        
+        </>
+
+    )
+}
